26ab46e80df58a6f500882c610580ba955143f20
rjsvcs <rjsvcs@rit.edu>
2019-02-28 10:49:43 -0500

    Added a couple of more activity solutions.

---
+++ src/threads/CountdownTimer.java
@@ -0,0 +1,24 @@
+package threads;
+
+public class CountdownTimer implements Runnable {
+    @Override
+    public void run() {
+        int time = -10;
+        while(true) {
+            String t = time < 0 ? "T" : "T+";
+            System.out.println(t + time);
+            time++;
+            try {
+                Thread.sleep(1000);
+            } catch(InterruptedException ie) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Runnable runnable = new CountdownTimer();
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+}
---
+++ src/threads/Joining.java
@@ -0,0 +1,21 @@
+package threads;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Joining {
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> threads = new ArrayList<>(5);
+        for(int i=0; i<5; i++) {
+            RunnableCounter counter = new RunnableCounter(i);
+            Thread thread = new Thread(counter);
+            thread.start();
+            threads.add(thread);
+        }
+
+        for(Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println(TextColor.DEFAULT.getANSII() + "Finished!");
+    }
+}
