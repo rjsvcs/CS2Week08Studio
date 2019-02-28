2fe076c00e92a9aa845d7d57fbdaaabd98dbd383
rjsvcs <rjsvcs@rit.edu>
2019-02-28 10:28:43 -0500

    Added text color to better differentiate between the different threads.

--- src/threads/TextColor.java
+++ src/threads/TextColor.java
@@ -1,29 +1,58 @@
 package threads;
 
+/**
+ * An enumeration that is used to keep track of ANSII codes used to change the
+ * color of printed text. Works inside IntelliJ but not from a terminal.
+ */
 public enum TextColor {
-    RESET("\u001B[0m"),
+    DEFAULT("\u001B[0m"), // ANSII reset; default color for the terminal
     RED("\u001B[31m"),
     GREEN("\u001B[32m"),
     YELLOW("\u001B[33m"),
     BLUE("\u001B[34m"),
     PURPLE("\u001B[35m"),
-    CYAN("\u001B[36m");
+    CYAN("\u001B[36m"),
+    GRAY("\u001B[37m");
 
+    /**
+     * Used to store all possible colors.
+     */
     private static final TextColor[] TEXT_COLORS = {
-            RESET, RED, GREEN, YELLOW, BLUE, PURPLE, CYAN
+            DEFAULT, RED, GREEN, YELLOW, BLUE, PURPLE, CYAN, GRAY
     };
 
+    /**
+     * The ANSII code for this color of text.
+     */
     private final String ansii;
 
+    /**
+     * Creates a new text color with the specified ANSII code.
+     *
+     * @param ansii The ANSII code for changing text color.
+     */
     TextColor(String ansii) {
         this.ansii = ansii;
     }
 
+    /**
+     * Returns the ANSII code for one of the possible colors based on the
+     * given number. Uses number % number of colors to determine which color
+     * to return.
+     *
+     * @param number A positive number.
+     * @return The ANSII code for the color as a string.
+     */
     public static String getColorString(int number) {
         int index = number % TEXT_COLORS.length;
         return TEXT_COLORS[index].getANSII();
     }
 
+    /**
+     * Returns the ANSII code for this text color.
+     *
+     * @return The ANSII code for this text color.
+     */
     public String getANSII() {
         return ansii;
     }
